6532bb20d631f1e1e296770831620ed2d188c062
joviprata <joviprata07@gmail.com>
2026-02-19 16:05:58 -0300

    avanços até aula 3-11 (controller, modules, client.rest)

---
+++ conceitos_nest/src/app/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+@Controller('home')
+export class AppController {
+  // constructor(private readonly appService: AppService) {}
+
+  @Get('hello')
+  getHello(): string {
+    return 'Qualquer coisa';
+  }
+
+  @Get('exemplo')
+  getExemplo(): string {
+    return 'Exemplo';
+  }
+}
